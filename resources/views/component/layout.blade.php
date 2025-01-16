1f02abacd464011e97734af5d7fc949511e273f1
Obaydul_Kuasha <obaydul00000@gmail.com>
2025-01-16 13:46:54 +0600

    edit my data all data

--- resources/views/component/layout.blade.php
+++ resources/views/component/layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Cyber-Handed</title>
+    <link rel="icon" href="{{asset('images/img/logo.jpg')}}" type="image/png/jpg?">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
